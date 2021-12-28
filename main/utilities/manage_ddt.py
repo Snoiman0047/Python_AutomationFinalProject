349d736ac0b09bec0f9452e9661459f908a80587
ramighawi <ramighawi10@gmail.com>
2021-12-28 20:49:45 +0200

    update manage_ddt with my path for config.xml

--- main/utilities/manage_ddt.py
+++ main/utilities/manage_ddt.py
@@ -5,6 +5,6 @@ import allure
 
 @allure.step('Get data from config_data.xml.')
 def get_data(node_name):
-    root = ET.parse('C:/Users/snoim/OneDrive/Desktop/Automation/Python/Python_AutomationFinalProject/configuration/data_config.xml').getroot()
+    root = ET.parse('C:/Automation/Python_AutomationFinalProject/configuration/data_config.xml').getroot()
     return root.find(".//" + node_name).text
 
